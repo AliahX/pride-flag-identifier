c4d593b74433381f2bf2a5e153b39597dd7df8dd
AliahX <40045439+AliahX@users.noreply.github.com>
2023-01-17 13:59:51 +1300

    finish most of the things
    finish most of the things apart from detecting which flag you've drawn

---
+++ flags.js
@@ -0,0 +1,324 @@
+flagList = {
+  "progress": [
+    {
+      "style": "progress",
+      "colors": [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "darkblue",
+        "purple",
+        "black",
+        "brown",
+        "lightblue",
+        "pink",
+        "white"
+      ]
+    }
+  ],
+  "agender": [
+    {
+      "style": "7-stripe",
+      "colors": [
+        "black",
+        "gray",
+        "white",
+        "lightgreen",
+        "white",
+        "gray",
+        "black"
+      ]
+    }
+  ],
+  "aromantic": [
+    {
+      "style": "5-stripe",
+      "colors": [
+        "green",
+        "lightgreen",
+        "white",
+        "gray",
+        "black"
+      ]
+    }
+  ],
+  "asexual": [
+    {
+      "style": "4-stripe",
+      "colors": [
+        "black",
+        "gray",
+        "white",
+        "purple"
+      ]
+    }
+  ],
+  "bisexual": [
+    {
+      "style": "3-stripe-bi",
+      "colors": [
+        "redpurple",
+        "lightpurple",
+        "darkblue"
+      ]
+    }
+  ],
+  "demiboy": [
+    {
+      "style": "7-stripe",
+      "colors": [
+        "darkgray",
+        "gray",
+        "lightblue",
+        "white",
+        "lightblue",
+        "gray",
+        "darkgray"
+      ]
+    }
+  ],
+  "demigirl": [
+    {
+      "style": "7-stripe",
+      "colors": [
+        "darkgray",
+        "gray",
+        "pink",
+        "white",
+        "pink",
+        "gray",
+        "darkgray"
+      ]
+    }
+  ],
+  "demiromantic": [
+    {
+      "style": "demi",
+      "colors": [
+        "white",
+        "green",
+        "gray",
+        "black"
+      ]
+    }
+  ],
+  "demisexual": [
+    {
+      "style": "demi",
+      "colors": [
+        "white",
+        "purple",
+        "gray",
+        "black"
+      ]
+    }
+  ],
+  "rainbow": [
+    {
+      "style": "6-stripe",
+      "colors": [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "darkblue",
+        "purple"
+      ]
+    }
+  ],
+  "genderfluid": [
+    {
+      "style": "5-stripe",
+      "colors": [
+        "pink",
+        "white",
+        "purple",
+        "black",
+        "darkblue"
+      ]
+    }
+  ],
+  "genderqueer": [
+    {
+      "style": "3-stripe",
+      "colors": [
+        "lavender",
+        "white",
+        "green"
+      ]
+    }
+  ],
+  "intersex": [
+    {
+      "style": "intersex",
+      "colors": [
+        "yellow",
+        "purple"
+      ]
+    }
+  ],
+  "lesbian": [
+    {
+      "style": "5-stripe",
+      "colors": [
+        "darkorange",
+        "lightorange",
+        "white",
+        "pink",
+        "redpurple"
+      ]
+    }
+  ],
+  "nonbinary": [
+    {
+      "style": "4-stripe",
+      "colors": [
+        "yellow",
+        "white",
+        "lavender",
+        "black"
+      ]
+    }
+  ],
+  "pansexual": [
+    {
+      "style": "3-stripe",
+      "colors": [
+        "redpurple",
+        "yellow",
+        "blue"
+      ]
+    }
+  ],
+  "polysexual": [
+    {
+      "style": "3-stripe",
+      "colors": [
+        "redpurple",
+        "palegreen",
+        "blue"
+      ]
+    }
+  ],
+  "transgender": [
+    {
+      "style": "5-stripe",
+      "colors": [
+        "lightblue",
+        "pink",
+        "white",
+        "pink",
+        "lightblue"
+      ]
+    }
+  ]
+}
+
+colorList = {
+  "red": "#ee2724",
+  "darkorange": "#d62902",
+  "orange": "#f46d1e",
+  "lightorange": "#ff9b54",
+  "yellow": "#FFD800",
+  "green": "#52bf37",
+  "palegreen": "#00d568",
+  "lightgreen": "#b8f483",
+  "darkblue": "#2851a9",
+  "blue": "#1d92f6",
+  "lightblue": "#88c4ed",
+  "redpurple": "#d60370",
+  "purple": "#9c158e",
+  "lightpurple": "#9b4f96",
+  "lavender": "#ab6cce",
+  "pink": "#f67cc5",
+  "brown": "#593219",
+  "black": "#000000",
+  "darkgray": "#7f7f7f",
+  "gray": "#a9a9a9",
+  "white": "#ffffff"
+}
+
+styleList = {
+  "3-stripe": [
+    "rect width='100%' height='33.33%' y='00.00%'",
+    "rect width='100%' height='33.33%' y='33.33%'",
+    "rect width='100%' height='33.33%' y='66.66%'"
+  ],
+  "3-stripe-bi": [
+    "rect width='100%' height='40.00%' y='00.00%'",
+    "rect width='100%' height='20.00%' y='40.00%'",
+    "rect width='100%' height='40.00%' y='60.00%'"
+  ],
+  "4-stripe": [
+    "rect width='100%' height='25.00%' y='00.00%'",
+    "rect width='100%' height='25.00%' y='25.00%'",
+    "rect width='100%' height='25.00%' y='50.00%'",
+    "rect width='100%' height='25.00%' y='75.00%'"
+  ],
+  "5-stripe": [
+    "rect width='100%' height='20.00%' y='00.00%'",
+    "rect width='100%' height='20.00%' y='20.00%'",
+    "rect width='100%' height='20.00%' y='40.00%'",
+    "rect width='100%' height='20.00%' y='60.00%'",
+    "rect width='100%' height='20.00%' y='80.00%'"
+  ],
+  "6-stripe": [
+    "rect width='100%' height='16.66%' y='00.00%'",
+    "rect width='100%' height='16.66%' y='16.66%'",
+    "rect width='100%' height='16.66%' y='33.32%'",
+    "rect width='100%' height='16.66%' y='49.98%'",
+    "rect width='100%' height='16.66%' y='66.64%'",
+    "rect width='100%' height='16.66%' y='83.30%'"
+  ],
+  "7-stripe": [
+    "rect width='100%' height='14.28%' y='00.00%'",
+    "rect width='100%' height='14.28%' y='14.28%'",
+    "rect width='100%' height='14.28%' y='28.56%'",
+    "rect width='100%' height='14.28%' y='42.84%'",
+    "rect width='100%' height='14.28%' y='57.12%'",
+    "rect width='100%' height='14.28%' y='71.40%'",
+    "rect width='100%' height='14.28%' y='85.68%'"
+  ],
+  "intersex": [
+    "rect width='100%' height='100.00%' y='00.00%'",
+    "circle cx='150' cy='100' r='50.00' fill='none' stroke-width='15.94'"
+  ],
+  "demi": [
+    "rect width='100%' height='40.00%' y='00.00%'",
+    "rect width='100%' height='20.00%' y='40.00%'",
+    "rect width='100%' height='40.00%' y='60.00%'",
+    "polygon points='0,0 0,200 114.75,100'"
+  ],
+  "progress": [
+    "rect width='100%' height='16.66%' y='00.00%'",
+    "rect width='100%' height='16.66%' y='16.66%'",
+    "rect width='100%' height='16.66%' y='33.32%'",
+    "rect width='100%' height='16.66%' y='49.98%'",
+    "rect width='100%' height='16.66%' y='66.64%'",
+    "rect width='100%' height='16.66%' y='83.30%'",
+    "polygon points='0,-50.39 0,250.39 141.55,100'",
+    "polygon points='0,-25.19 0,225.19 118.05,100'",
+    "polygon points='0,00.00 0,200.00 95.65,100'",
+    "polygon points='0,25.19 0,174.81 71.05,100'",
+    "polygon points='0,50.39 0,149.61 47.55,100'",
+  ]
+}
+
+
+function generateFlag(style, colors, strip=false) {
+  SVGData = "";
+  styleList[style].forEach(function(element, i) {
+    if(element.split(" ")[0] == "circle") {
+      element = element + "stroke='" + (colors[i] == undefined ? "#8C8273" : colorList[colors[i]]) + "'"
+    } else {
+      element = element + " fill='" + (colors[i] == undefined ? "#1E2021' stroke='#8C8273" : colorList[colors[i]]) + "'"
+    }
+    SVGData += "\n  <" + element + " />";
+  });
+  if(strip) {
+    return SVGData;
+  } else {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${SVGData}\n</svg>`
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,85 @@
+if(localStorage.getItem("page") == null) localStorage.setItem("page", 0);
+color = "#ffffff"
+
+
 function setPage(index) {
-  
+  $("#mainBox").css("display", index == 0 ? "block" : "none");
+  $("#createBox").css("display", index == 1 ? "block" : "none");
+  $("#allBox").css("display", index == 2 ? "block" : "none");
+  localStorage.setItem("page", index);
+}
+
+function search(input) {
+  $(".flagBox").each(function() {
+    e = $(this);
+    e.find(".flagName").text().toLowerCase().includes(input.value.toLowerCase()) ? e.show() : e.hide();
+  });
+}
+
+function setColor(col) {
+  $(".colorButton").removeClass("active");
+  $(`button[onclick="setColor('${col}')"]`).addClass("active");
+  color = colorList[col];
+}
+
+function setFlagStyle(self) {
+  flagElement = document.querySelector("#drawFlag");
+  flagElement.innerHTML = generateFlag(self.value, [], true);
+  document.querySelectorAll("#drawFlag *").forEach(function(element) {
+    element.onclick = function() {
+      if(element.tagName == "circle") {
+        element.style.stroke=color;
+      } else {
+        element.style.fill=color;
+      }
+    }
+  });
+}
+
+function load() {
+  Object.entries(flagList).forEach(([name, flag]) => {
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    document.querySelector("#allBox").innerHTML += `<div class="contentBox flagBox">
+    <div class="flagName">${name}</div>
+    ${generateFlag(flag[0]["style"], flag[0]["colors"])}
+  </div>`
+  });
+
+  Object.entries(styleList).forEach(([name]) => {
+    document.querySelector("#flagStyle").innerHTML += `<option value="${name}">${name.charAt(0).toUpperCase() + name.slice(1)}</option>`
+  });
+
+  styleCount = {};
+  Object.entries(flagList).forEach(([name, flag]) => {
+    if(styleCount[flag[0]["style"]] == undefined) {
+      styleCount[flag[0]["style"]] = 1;
+    } else {
+      styleCount[flag[0]["style"]] += 1;
+    }
+  });
+
+  document.querySelector("#flagStyle").value = Object.keys(styleCount).reduce((a, b) => styleCount[a] > styleCount[b] ? a : b);
+  document.querySelector("#flagStyle").oninput();
+
+  Object.entries(colorList).forEach(([name, color], i) => {
+    document.querySelector("#colorBox").innerHTML += `<button class="colorButton" style="background-color: ${color};" onclick="setColor('${name}')"></button>`
+    if (i % 5 == 4) {
+      document.querySelector("#colorBox").innerHTML += `<br />`
+    }
+  });
+
+  colorCount = {};
+  Object.entries(flagList).forEach(([name, flag]) => {
+    flag[0]["colors"].forEach(function(element, i) {
+      if(colorCount[element] == undefined) {
+        colorCount[element] = 1;
+      } else {
+        colorCount[element] += 1;
+      }
+    });
+  });
+
+  color = colorList[Object.keys(colorCount).reduce((a, b) => colorCount[a] > colorCount[b] ? a : b)];
+
+  document.querySelector(`.colorButton[style="background-color: ${colorList[Object.keys(colorCount).reduce((a, b) => colorCount[a] > colorCount[b] ? a : b)]};"]`).classList.add("active");
 }
